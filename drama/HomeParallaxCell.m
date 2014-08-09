2ab21a8af12cf0e791ff1d0b5000c0e5efbfe681
qrshan <qrshan@sina.com>
2014-08-09 16:51:25 +0800

    添加首页列表

---
+++ drama/HomeParallaxCell.m
@@ -0,0 +1,34 @@
+//
+//  HomeParallaxCell.m
+//  drama
+//
+//  Created by qrs on 14-8-9.
+//  Copyright (c) 2014年 qrs. All rights reserved.
+//
+
+#import "HomeParallaxCell.h"
+
+@implementation HomeParallaxCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)awakeFromNib
+{
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
